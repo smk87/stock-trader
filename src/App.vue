c29b498d9f18f0c063b3258628bbf4776d199ee5
smk87 <sqlnmhmdkhanemon@gmail.com>
2019-10-05 14:17:41 +0600

    ADD: Header comp.

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,22 @@
 <template>
-  <div class="container"></div>
+  <div class="container">
+    <app-header></app-header>
+    <div class="row">
+      <div class="col-xs-12">
+        <router-view></router-view>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {};
+import Header from "./components/Header.vue";
+
+export default {
+  components: {
+    appHeader: Header
+  }
+};
 </script>
 
 <style>
---
+++ src/components/Header.vue
@@ -0,0 +1,28 @@
+<template>
+  <ul class="nav nav-pills mt-5">
+    <li class="nav-item">
+      <router-link activeClass="active" to="/">Stock Trader</router-link>
+    </li>
+    <!-- <li class="nav-item dropdown"></li>
+      <a
+        class="nav-link dropdown-toggle"
+        data-toggle="dropdown"
+        href="#"
+        role="button"
+        aria-haspopup="true"
+        aria-expanded="false"
+        >Dropdown</a
+      >
+      <div class="dropdown-menu">
+        <a class="dropdown-item" href="#">Save Data</a>
+        <a class="dropdown-item" href="#">Load Data</a>
+      </div>
+    </li> -->
+    <li class="nav-item">
+      <router-link to="/portfolio" activeClass="active">Portfolio</router-link>
+    </li>
+    <li class="nav-item">
+      <router-link to="/stocks" activeClass="active">Stocks</router-link>
+    </li>
+  </ul>
+</template>

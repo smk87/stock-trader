faeef8be07bd262fadddaf2b05536be49dc72075
smk87 <sqlnmhmdkhanemon@gmail.com>
2019-10-12 14:16:51 +0600

    CONNECT: Store with Portfolio and Stocks.

--- src/components/portfolio/Portfolio.vue
+++ src/components/portfolio/Portfolio.vue
@@ -1,58 +1,20 @@
 <template>
-  <div class="col-sm-6 col-md-4">
-    <div class="panel panel-success">
-      <div class="panel-heading">
-        <h3 class="panel-title">
-          {{ stock.name
-          }}<small>
-            (Price: {{ stock.price }} | Quantity: {{ stock.quantity }})</small
-          >
-        </h3>
-      </div>
-      <div class="panel-body">
-        <div class="pull-left">
-          <input
-            type="number"
-            class="form-control"
-            placeholder="Quantity"
-            v-model.number="quantity"
-          />
-        </div>
-        <div class="pull-right">
-          <button
-            @click="sellStock"
-            :disabled="quantity <= 0 || !Number.isInteger(quantity)"
-            class="btn btn-success"
-          >
-            Sell
-          </button>
-        </div>
-      </div>
-    </div>
+  <div>
+    <app-stock v-for="stock in stocks" :stock="stock"></app-stock>
   </div>
 </template>
 
 <script>
-import { mapActions } from "vuex";
+import { mapGetters } from "vuex";
+
+import Stock from "./Stock.vue";
 
 export default {
-  props: ["stock"],
-  data() {
-    return {
-      quantity: 0
-    };
+  computed: {
+    ...mapGetters({ stocks: "stockPortfolio" })
   },
-  methods: {
-    ...mapActions(["sellStock"]),
-    sellStock() {
-      const order = {
-        stockId: this.stock.id,
-        stockPrice: this.stock.price,
-        quantity: this.quantity
-      };
-
-      this.sellStock();
-    }
+  components: {
+    appStock: Stock
   }
 };
 </script>

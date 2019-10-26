c1a3ef5a2c4ef283dc3a585bcd9d89eb42f514b0
Saqlain Mahmud <30999133+smk87@users.noreply.github.com>
2019-10-26 13:09:34 +0600

    ADD: fund in Header.vue

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -3,26 +3,29 @@
     <li class="nav-item">
       <router-link activeClass="active" to="/">Stock Trader</router-link>
     </li>
-    <!-- <li class="nav-item dropdown"></li>
-      <a
-        class="nav-link dropdown-toggle"
-        data-toggle="dropdown"
-        href="#"
-        role="button"
-        aria-haspopup="true"
-        aria-expanded="false"
-        >Dropdown</a
-      >
-      <div class="dropdown-menu">
-        <a class="dropdown-item" href="#">Save Data</a>
-        <a class="dropdown-item" href="#">Load Data</a>
-      </div>
-    </li> -->
+    <!-- -->
     <li class="nav-item">
       <router-link to="/portfolio" activeClass="active">Portfolio</router-link>
     </li>
     <li class="nav-item">
       <router-link to="/stocks" activeClass="active">Stocks</router-link>
     </li>
+    <p class="navbar-text navbar-right">Funds: {{ funds }}</p>
+    <li class="nav-item navbar-right">
+      <router-link to="#" activeClass="active">Save Data</router-link>
+    </li>
+    <li class="nav-item navbar-right">
+      <router-link to="#" activeClass="active">Load Data</router-link>
+    </li>
   </ul>
 </template>
+
+<script>
+export default {
+  computed: {
+    funds() {
+      return this.$store.getters.funds;
+    }
+  }
+};
+</script>

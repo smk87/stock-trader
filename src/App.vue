1816daa5702461bef87db922285de627a2b631ce
Saqlain Mahmud <30999133+smk87@users.noreply.github.com>
2019-12-06 20:22:17 +0600

    ADD: transition animation to page navigation.

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,9 @@
     <app-header></app-header>
     <div class="row">
       <div class="col-xs-12">
-        <router-view></router-view>
+        <transition name="slide" mode="out-in">
+          <router-view></router-view>
+        </transition>
       </div>
     </div>
   </div>
@@ -23,4 +25,33 @@ export default {
 </script>
 
 <style>
+.slide-enter-active {
+  animation: slide-in 200ms ease-out forwards;
+}
+
+.slide-leave-active {
+  animation: slide-out 200ms ease-out forwards;
+}
+
+@keyframes slide-in {
+  from {
+    transform: translateY(-30px);
+    opacity: 0;
+  }
+  to {
+    transform: translateY(0);
+    opacity: 1;
+  }
+}
+
+@keyframes slide-out {
+  from {
+    transform: translateY(0);
+    opacity: 1;
+  }
+  to {
+    transform: translateY(-30px);
+    opacity: 0;
+  }
+}
 </style>
